d21e21147e03978cf6e5f37ac1890375f3201ded
t-kupp <kupper.thorge@gmail.com>
2024-10-26 11:41:44 +0200

    q7 done

--- question7.js
+++ question7.js
@@ -2,7 +2,14 @@
 // bort vokaler, alltså bokstäverna: aouåeiyäö.
 
 function removeVowels(word) {
-  //din kod här
+  const vowels = ["a", "o", "u", "å", "e", "i", "y", "ä", "ö"];
+  let string = "";
+  for (char of word) {
+    if (vowels.find((vowel) => vowel == char) == undefined) {
+      string += char;
+    }
+  }
+  return string;
 }
 
 console.log(removeVowels("hej")); // Ska logga "hj"
