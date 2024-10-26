1cd3689fd08adbc75fdf1ea7b19ad25cf6b77647
t-kupp <kupper.thorge@gmail.com>
2024-10-26 11:28:26 +0200

    q6 done

--- question6.js
+++ question6.js
@@ -1,2 +1,13 @@
 // 4 Skriv en funktion som tar en array med strängar som input och
 // returnerar en array med bara de strängar som är minst 4 tecken långa.
+const strings = ["hello", "hallo", "bonjour", "hej"];
+
+function filterStrings(arr) {
+  let newArray = [];
+  arr.forEach((string) => {
+    if (string.length >= 4) newArray.push(string);
+  });
+  return newArray;
+}
+
+console.log(filterStrings(strings));
