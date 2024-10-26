b6b93bac4f6b08e4f4381ab332e00c5a29ff000c
t-kupp <kupper.thorge@gmail.com>
2024-10-26 11:00:55 +0200

    q2 done

--- question2.js
+++ question2.js
@@ -1,8 +1,13 @@
 // 2.	Skriv klart funktionen findLargest.
 // findLargest ska returnera det största numret i arrayen som skickas till den:
-
 function findLargest(numbers) {
-  // lägg till din kod här
+  let highestNumber = numbers[0];
+  for (n of numbers) {
+    if (n > highestNumber) {
+      highestNumber = n;
+    }
+  }
+  return highestNumber;
 }
 
 console.log(findLargest([1, 2, 3, 4, 5])); // ska logga 5
@@ -10,9 +15,14 @@ console.log(findLargest([-1, -2, -3, -4, -5])); // ska logga -1
 
 // 2.1	Skriv klart funktionen findSmallest.
 // findSmallest ska returnera det minsta talet i arrayen.
-
 function findSmallest(numbers) {
-  // lägg till din kod här
+  let smallestNumber = numbers[0];
+  for (n of numbers) {
+    if (n < smallestNumber) {
+      smallestNumber = n;
+    }
+  }
+  return smallestNumber;
 }
 
 console.log(findSmallest([1, 2, 3, 4, 5])); // ska logga 1
