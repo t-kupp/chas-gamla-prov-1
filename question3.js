5702244fb5643e239a6f48f71df2d5fe2e7e5b4f
t-kupp <kupper.thorge@gmail.com>
2024-10-26 11:06:02 +0200

    q3 done

--- question3.js
+++ question3.js
@@ -1,9 +1,8 @@
 // 3. Samma ord?
 // Skriv klart funktionen som kollar om två ord är likadana
 // oberoende av stor och liten bokstav.
-
 function compareWords(word1, word2) {
-  //din kod här
+  return word1.toLowerCase() == word2.toLowerCase();
 }
 
 console.log(compareWords("hej", "HEJ")); // Ska logga true
